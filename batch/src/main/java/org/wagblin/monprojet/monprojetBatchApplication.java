325f621380beefdf17b8f5f055e9848c01729980
wagblin3 <wagblin@icloud.com>
2025-09-03 20:04:10 +0200

    update batch

--- batch/src/main/java/org/wagblin/monprojet/monprojetBatchApplication.java
+++ batch/src/main/java/org/wagblin/monprojet/monprojetBatchApplication.java
@@ -1,7 +1,10 @@
 package org.wagblin.monprojet;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+@SpringBootApplication
 public class monprojetBatchApplication {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
